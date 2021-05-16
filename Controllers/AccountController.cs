29237ca7a8fb8290b76fe7f1f04eb8ec5fe66ecd
kapilsh06 <84267938+kapilsh06@users.noreply.github.com>
2021-05-16 15:01:22 +0530

    Asp Core 5 Validation
    Server side validation - Explicit Model Validation (Commented in AccountController), DataAnnotation Model validation 
    Client side validation - JavaScript, Third Party Library Framework.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Validation_WithMVC.Models;
+
+namespace Validation_WithMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult SignUp()
+        {
+            FillOnLoad();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SignUp(UserModel model)
+        {
+
+            //****---------- Explicit Model Validation -----------****//
+            //if (string.IsNullOrEmpty(model.name))
+            //{
+            //    ModelState.AddModelError("Name", "Enter Name");
+            //}
+            //if (string.IsNullOrEmpty(model.password))
+            //{
+            //    ModelState.AddModelError("password", "Enter Password");
+            //}
+            //if (string.IsNullOrEmpty(model.confirmpassword))
+            //{
+            //    ModelState.AddModelError("confirmpassword", "Enter Confirm Password");
+            //}
+            //if (string.IsNullOrEmpty(model.address))
+            //{
+            //    ModelState.AddModelError("address", "Enter Address");
+            //}
+            //if (string.IsNullOrEmpty(model.contactno))
+            //{
+            //    ModelState.AddModelError("contactno", "Enter Contact No");
+            //}
+            //if (model.terms == false)
+            //{
+            //    ModelState.AddModelError("terms", "Please Checked terms");
+            //}
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Message");
+            }
+            return View();
+        }
+
+
+        public IActionResult Message()
+        {
+            return View();
+        }
+
+        public void fillCountry()
+        {
+            var list = new List<SelectListItem>
+                {
+                    new SelectListItem{ Text="India", Value = "1",Selected = true },
+                    new SelectListItem{ Text="China", Value = "2" },
+                    new SelectListItem{ Text="America", Value = "3" },
+                    new SelectListItem{ Text="Pakistan", Value = "4" },
+                };
+            ViewData["Country"] = list;
+
+        }
+
+        public void fillCity()
+        {
+            var list = new List<SelectListItem>
+                {
+                    new SelectListItem{ Text="Jaipur", Value = "1",Selected = true },
+                    new SelectListItem{ Text="Ajmer", Value = "2" },
+                    new SelectListItem{ Text="Delhi", Value = "3" },
+                    new SelectListItem{ Text="Noida", Value = "4" },
+                    new SelectListItem{ Text="Aligad", Value = "2" },
+                    new SelectListItem{ Text="Raj Nagar", Value = "3" },
+                    new SelectListItem{ Text="Rampur", Value = "4" },
+
+                };
+            ViewData["City"] = list;
+        }
+
+        public void FillOnLoad()
+        {
+            fillCountry();
+            ViewData["Gender"] = new List<string>() { "Male", "Female" };
+        }
+
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Validation_WithMVC.Attributes;
+
+namespace Validation_WithMVC.Models
+{
+    public class UserModel
+    {
+        [Required(ErrorMessage = "Please enter name"), MaxLength(50)]
+        public string Name { get; set; }
+
+
+        [Required(ErrorMessage = "Please enter user name"), MaxLength(50)]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Please enter password"), MaxLength(15)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Please enter confirm password"), MaxLength(15), Display(Name= "Confirm Password")]
+        [Compare("Password", ErrorMessage = "Confirm password doesn't match")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "Please enter contact no"), MaxLength(10)]
+        [RegularExpression("^[6,7,8,9]\\d{9}$", ErrorMessage = "Wrong contact number")]
+        public string Contact{ get; set; }
+
+
+        [Required(ErrorMessage = "Please enter country")]
+        public string Country { get; set; }
+
+        [Required(ErrorMessage = "Please enter city")]
+        public string City { get; set; }
+       
+        public string Gender { get; set; }
+
+        //  [Required(ErrorMessage = "Please enter address")]
+        //  public string address { get; set; }
+
+        [ValidationCheckbox(ErrorMessage = "Please check terms"), Display(Name = "Accept Terms")]
+        public bool AcceptTerms { get; set; }
+    }
+}
